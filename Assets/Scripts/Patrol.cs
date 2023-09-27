03294b962e5e7d00029c8f7b3c5b945ffd14c8a6
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2023-09-27 16:49:14 -0500

    Basic AI Patrol

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Patrol : MonoBehaviour
+{
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {        
+        if (points.Length == 0) return;
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+        Debug.Log("Next point succeded");
+    }
+
+    IEnumerator StayInPosition()
+    {
+        agent.isStopped = true;
+        agent.speed = 0f;
+        yield return new WaitForSeconds(5);
+        agent.isStopped = false; 
+        agent.speed = 3f;
+        Debug.Log("I worked");
+    }
+
+    private void FixedUpdate()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            if (gameObject.CompareTag("Janitor"))
+            {                
+                StartCoroutine(StayInPosition());
+                Debug.Log("I finish my patrol");
+            }
+            GotoNextPoint();
+
+        }
+    }
+
+}
