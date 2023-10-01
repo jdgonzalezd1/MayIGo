1960f4eeb1211963fa3ae0763934fa0682fbba7e
byronGH <byronsalgadoval@gmail.com>
2023-09-30 19:14:44 -0500

    Added characters

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -1,11 +1,17 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
+
 
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Patrol : MonoBehaviour
 {
+    //Gets the animator to change the Janitor's animations
+    public Animator myAnim;
+
     public Transform[] points;
     private int destPoint = 0;
     private NavMeshAgent agent;
@@ -13,6 +19,9 @@ public class Patrol : MonoBehaviour
 
     private void Start()
     {
+        //gets the children's animator
+        myAnim = GetComponentInChildren<Animator>();
+
         agent = GetComponent<NavMeshAgent>();
         //agent.autoBraking = false;
 
@@ -20,21 +29,29 @@ public class Patrol : MonoBehaviour
     }
 
     void GotoNextPoint()
-    {        
+    {
+        myAnim.Play("mixamo_com");
         if (points.Length == 0) return;
         agent.destination = points[destPoint].position;
         destPoint = (destPoint + 1) % points.Length;
         Debug.Log("Next point succeded");
+        
+
     }
 
     IEnumerator StayInPosition()
     {
+        
         agent.isStopped = true;
+        myAnim.Play("Look Around");
         agent.speed = 0f;
         yield return new WaitForSeconds(stopTime);
         agent.isStopped = false; 
         agent.speed = 3f;
         Debug.Log("I worked");
+        
+
+
     }
 
     private void FixedUpdate()
@@ -45,6 +62,8 @@ public class Patrol : MonoBehaviour
             {                
                 StartCoroutine(StayInPosition());
                 Debug.Log("I finish my patrol");
+
+                
             }
             GotoNextPoint();
 
